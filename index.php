67684b7850edb34108cf04ec017b735ad7d7b6b7
Ishika102 <58440964+Ishika102@users.noreply.github.com>
2020-11-09 00:06:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,11 @@
 </div>
 <img src="nationalherald_2019-06_ccd3bca4-b383-471e-90f2-6521bc2d7b72_Railways_PT.jpg" alt="Indian Railways" width="500" height="333">
 
+<footer>
+  <p><a href="Admin.php">Admin</a></p>
+  <p><a href="index.php">Home</a></p>
+  <p><a href="About.php">About</a>
+  <p><a href="contact.php">Contact</a></p>
+</footer>
 </body>
 </html>

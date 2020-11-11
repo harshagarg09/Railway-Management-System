d3774800919be913f43a8cdaf5dd93fbe433ba63
harshagarg09 <39459950+harshagarg09@users.noreply.github.com>
2020-11-12 01:48:10 +0530

    Create user-header.php

---
+++ user-header.php
@@ -0,0 +1,38 @@
+
+<style>
+body {
+  margin: 10;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #332;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 20px 30px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+
+<div class="topnav">
+  <a href="index.php">Home</a>
+  <a href="About.php">About</a>
+  <a href="contact.php">Contact</a>
+  <a href="logout.php"  style="float:right">Logout</a>
+</div>

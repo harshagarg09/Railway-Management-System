36fed03e35a6b959eeb85361bdfd12f0f3fe6a7e
Ishika102 <58440964+Ishika102@users.noreply.github.com>
2020-11-20 23:57:40 +0530

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,1 +1,12 @@
 <?php session_start();?>
+<?php include('header.php') ?>
+
+            <form action="trainAdd.php" method="post" onsubmit="return confirm('Are you sure you want to submit?')">
+                <input type="text" name="name" placeholder="Enter train no" required>
+                <input type="date" name="date" placeholder="Enter Date" required>
+                <input type="text" name="source" placeholder="Enter Source Station" required>
+				<input type="text" name="name" placeholder="Enter destination Station" required>
+                <input class="btn" type="submit" name="submit" value="Submit">
+            </form>
+</body>
+</html>

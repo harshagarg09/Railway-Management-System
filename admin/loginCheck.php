923573c4cd6a486367006548f264b799c1739ff1
Ishika102 <58440964+Ishika102@users.noreply.github.com>
2020-11-20 23:09:07 +0530

    Update loginCheck.php

--- admin/loginCheck.php
+++ admin/loginCheck.php
@@ -1,23 +1,24 @@
+
 <?php
+	include('connection.php');
+	
     if(isset($_POST['login'])){
        
         $username = $_POST['username'];
         $password = $_POST['password'];
         
         
-        $check_user = " SELECT * FROM admin WHERE username = '$username' AND password = '$password' ";
+        $check_user = " SELECT * FROM admin WHERE db_username = '$username' AND db_password = '$password' ";
                 
-        $result = mysqli_query($conn, $check_password);
+        $result = mysqli_query($conn, $check_user);
         
         if(mysqli_num_rows($result)>0)
 		{
-            $_SESSION['username'] = $username;
-            header('location:Dashboard.php');
+            header('location:dashboard.php');
         }
         else{
             header('location:../Admin.php');
         }
            
-        }
     }
 ?>

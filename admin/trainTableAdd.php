831ab418b89a10039b0f4cc592842aafc718a6e1
Ishika102 <58440964+Ishika102@users.noreply.github.com>
2020-11-23 14:21:05 +0530

    Update trainTableAdd.php

--- admin/trainTableAdd.php
+++ admin/trainTableAdd.php
@@ -24,17 +24,33 @@
 			<?php echo "Entered the Wrong Date";
 			?></h2>
 			<?php
+			//header('location:trainTableAdd.php');
+			die();
 		}
-		
+		$check_user = " SELECT * FROM traincoach WHERE db_trainNo = '$trainNo' and db_date ='$date' ";
+                
+        $res1 = mysqli_query($conn, $check_user);
+        
+        if(mysqli_num_rows($res1)>0)
+		{
+			?>
+			<h2> "Already the train with the given number exists";
+			</h2>
+			<?php
+			die();
+			//header('location:trainTableAdd.php');
+        }
 		
 		$query = "INSERT INTO `traincoach` (`db_trainNo`,`db_AC`,`db_Sleeper`,`db_date`) VALUES ('$trainNo','$ACcoach','$SleeperCoach','$date')";
 					
 		$res = mysqli_query($conn,$query);
 		if (!$res) {
-			die('Invalid query: ' . mysqli_error());
+			
+			die('Invalid query: ' . mysqli_error($conn));
 		}
 		
-		
+		if (mysqli_query($conn, $sqlq)) {
+			
 			for($j = 1;$j<=$ACcoach;$j++){
 				
 				for($i = 1; $i<19; $i++) {
@@ -45,7 +61,7 @@
 				
 					if (!$result) {
 						echo mysqli_error($conn);
-					//	die();
+						die();
 					}
 				}
 			}
@@ -59,11 +75,15 @@
 				
 					if (!$result) {
 						echo mysqli_error($conn);
-						//die();
+						die();
 					}
 				}
 			}
 		
+		} 
+		else {
+		  echo "Error : " . mysqli_error($conn);
+		}
 		header('location:success.php');
     }
 	else

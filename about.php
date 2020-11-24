a6fdd8efbd21eabc0102fd6ae391376dd0d5cf98
Ishika102 <58440964+Ishika102@users.noreply.github.com>
2020-11-24 16:57:39 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,5 +1,31 @@
 <?php include('header.php'); ?>
 <?php include('connection.php'); ?>
-<h2>About Page</h2>
+<br>
+<div>
+	<marquee behavior="scroll" direction="left" height="10%" scrollamount="10">
+		<h2>Welcome to Indian Railways</h2>
+	</marquee>
+</div>
+<br>
+<table id="customers">
+  <tr>
+    <th>Trains</th>
+    <th>No of Sleeper Coach</th>
+    <th>No of AC Coach</th>
+  </tr>
+  <?php 
+	$qur="SELECT * FROM `train`";
+	$res=mysqli_query($conn,$qur); 
+	if(mysqli_num_rows($res)>0){  
+		while($data=mysqli_fetch_array($res)){ ?>
+			<tr>
+			<td><?php echo $data['db_trainNo']; ?></td>
+			<td><?php echo $data['db_Sleeper']; ?></td>
+			<td><?php echo $data['db_AC']; ?></td>
+			</tr>	
+	<?php }  
+	} ?>  
+</table>
+
 </body>
 </html>

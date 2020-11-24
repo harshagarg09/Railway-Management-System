ef51a4f2bca907391e9fd9bd7c1ff2afcc7f9097
Ishika102 <58440964+Ishika102@users.noreply.github.com>
2020-11-24 18:32:11 +0530

    Update Admin.php

--- admin/Admin.php
+++ admin/Admin.php
@@ -1,5 +1,33 @@
  <?php include_once('connection.php'); ?>
  <?php include_once('header.php'); ?>
+<?php
+	//include('connection.php');
+    if(isset($_POST['login'])){
+       
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        
+        $check_user = " SELECT * FROM admin WHERE db_username = '$username' AND db_password = '$password' ";
+                
+        $result = mysqli_query($conn, $check_user);
+        
+        if(mysqli_num_rows($result)>0)
+		{
+            header('location:dashboard.php');
+        }
+        else{
+			?>
+			<?php
+			$_SESSION['status'] ="Wrong UserName/Password";
+			$_SESSION['text'] = "Try Again";
+			$_SESSION['status_code'] = "Warning";
+			echo "Error";
+            //header('location:admin.php');
+        }
+           
+    }
+?>
 <div class="container">
         <div class="login">
             <form action="loginCheck.php" method="post">
@@ -14,3 +42,4 @@
 
 </body>
 </html>
+<?php include('scripts.php');?>
